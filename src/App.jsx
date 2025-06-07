6c8f2db765bc614ed7f9f92717923b468224c335
RockPaddy <s2104@oita.kosen-ac.jp>
2025-06-07 14:48:09 +0900

    Add: Added a caution.

--- src/App.jsx
+++ src/App.jsx
@@ -55,8 +55,9 @@ export const App = () => {
       <div id="down-trains"><TrainTimetableDisplay direction="下り" fileName="DownTrains" /></div>
       <ul>
         <li><em>平日の時刻を表示しています．</em></li>
+        <li><em>ダイヤ乱れなどのリアルタイムの運行状況には対応していません．</em></li>
         <li>本サービスはJR九州およびその関連会社と全く関係ありません．</li>
-        <li>連絡先: RockPaddy，<a href="https://docs.google.com/forms/d/e/1FAIpQLSdCSEvJwwkPuxDm12P8cGlS0Q8-p1VABZ802VRUcrsXCLCLEA/viewform?usp=dialog">連絡フォーム．</a></li>
+        <li>連絡先: RockPaddy，<a href="https://docs.google.com/forms/d/e/1FAIpQLSdCSEvJwwkPuxDm12P8cGlS0Q8-p1VABZ802VRUcrsXCLCLEA/viewform?usp=dialog">連絡フォーム</a>．</li>
       </ul>
     </div>
   );
